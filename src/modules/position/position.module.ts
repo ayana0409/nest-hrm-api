b796e263b851db361eed4dbfd4d719725dd5c5da
ayana0409 <duongdoanthuan2003@gmail.com>
2025-09-25 15:03:11 +0700

    fix dependence

--- src/modules/position/position.module.ts
+++ src/modules/position/position.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PositionService } from './position.service';
 import { PositionController } from './position.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Position, PositionSchema } from './schema/position.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Position.name, schema: PositionSchema }]),
+  ],
   controllers: [PositionController],
   providers: [PositionService],
 })
