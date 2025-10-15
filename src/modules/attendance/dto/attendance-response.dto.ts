5c823125307d41d678cb016c959f01bb160ddd20
ayana0409 <duongdoanthuan2003@gmail.com>
2025-10-15 09:41:29 +0700

    format date response

--- src/modules/attendance/dto/attendance-response.dto.ts
+++ src/modules/attendance/dto/attendance-response.dto.ts
@@ -1,28 +1,33 @@
-import { Expose, Transform } from "class-transformer";
+import { DateHelper } from '@/common/helpers/dateHelper';
+import { Expose, Transform } from 'class-transformer';
 
 export class AttendanceResponseDto {
-    @Expose()
-    @Transform(({ obj }) => obj._id?.toString())
-    id: string;
+  @Expose()
+  @Transform(({ obj }) => obj._id?.toString())
+  id: string;
 
-    @Expose()
-    date: Date;
+  @Expose()
+  date: Date;
 
-    @Expose()
-    checkIn?: Date;
+  @Expose()
+  @Transform(({ obj }) => (obj.checkIn ? DateHelper.format(obj.checkIn) : null))
+  checkIn?: Date;
 
-    @Expose()
-    checkOut?: Date;
+  @Expose()
+  @Transform(({ obj }) =>
+    obj.checkOut ? DateHelper.format(obj.checkOut) : null,
+  )
+  checkOut?: Date;
 
-    @Expose()
-    status?: string;
+  @Expose()
+  status?: string;
 
-    @Expose()
-    note?: string;
+  @Expose()
+  note?: string;
 
-    @Expose()
-    createdAt: Date;
+  @Expose()
+  createdAt: Date;
 
-    @Expose()
-    updatedAt: Date;
+  @Expose()
+  updatedAt: Date;
 }
